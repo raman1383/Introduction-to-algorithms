7fbb3c4e480112558fa11074e0ab5b590de3cb09
raman1383 <raman83shariati@gmail.com>
2022-07-16 21:34:20 +0430

    r

--- COOL STUFF/C++/main.cpp
+++ COOL STUFF/C++/main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int arr[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for (int i = 0; i < 9; i++)
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
     {
         printf("%d\n", arr[i]);
     }
