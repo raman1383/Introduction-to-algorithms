be78ee5d1df13da8c581aa068f6d954e63fbed6d
raman1383 <raman83shariati@gmail.com>
2022-07-16 16:59:01 +0430

    r

---
+++ COOL STUFF/C++/lin.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+constexpr int MAX = 100;
+
+namespace lin
+{
+    void fucker()
+    {
+        for (int i = 0; i < MAX; i++)
+        {
+            std::cout << "Lets fuck >>>" << std::endl;
+        }
+    }
+
+} // namespace lin
--- COOL STUFF/C++/main.cpp
+++ COOL STUFF/C++/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "lin.hpp"
 using std::cout;
 constexpr int double_it(int arg) { return arg * arg; };
 
@@ -6,5 +7,6 @@ int main()
 {
     int bad_result = ((4 + 1) * (4 + 1));
     cout << bad_result;
+    lin::fucker();
     return 0;
 }
