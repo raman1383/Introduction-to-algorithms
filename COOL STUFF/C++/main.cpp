d8019e7a4b4d9cd8b16d7f913a1421c5fe6bf39a
raman1383 <raman83shariati@gmail.com>
2022-07-16 16:46:18 +0430

    r

--- COOL STUFF/C++/main.cpp
+++ COOL STUFF/C++/main.cpp
@@ -1,46 +1,10 @@
 #include <iostream>
-#include <vector>
+using std::cout;
+constexpr int double_it(int arg) { return arg * arg; };
 
 int main()
 {
-    int n;
-    bool swapCheck = true;
-    std::cout << "Enter the amount of number so sort : \n";
-    std::cin >> n;
-    std::vector<int> numbers;
-    std::cout << "Enter " << n << " numbers :\n";
-    int num;
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> num;
-        numbers.push_back(num);
-    }
-
-    for (int i = 0; (i < n) && (swapCheck); i++)
-    {
-        swapCheck = false;
-        for (int j = 0; j < n - 1 - i; j++)
-        {
-            if (numbers[j] > numbers[j + 1])
-            {
-                swapCheck = true;
-                std::swap(numbers[j], numbers[j + 1]);
-            }
-        }
-    }
-
-    // Output
-    std::cout << "\nSorted Array : ";
-    for (int i = 0; i < numbers.size(); i++)
-    {
-        if (i != numbers.size() - 1)
-        {
-            std::cout << numbers[i] << ", ";
-        }
-        else
-        {
-            std::cout << numbers[i] << std::endl;
-        }
-    }
+    int bad_result = ((4 + 1) * (4 + 1));
+    cout << bad_result;
     return 0;
 }
