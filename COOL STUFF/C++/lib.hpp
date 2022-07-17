bd818ae1d0976b4bbcd6846179e863ac44a5f359
raman1383 <raman83shariati@gmail.com>
2022-07-17 16:09:44 +0430

    r

--- COOL STUFF/C++/lib.hpp
+++ COOL STUFF/C++/lib.hpp
@@ -6,5 +6,15 @@ using std::cout, std::endl, std::vector;
 template <typename T>
 T biggest(T first, T second)
 {
+    cout << "calling first impl\n";
     return (first < second) ? second : first;
 }
+template <typename T, typename U>
+T biggest(T first, U second)
+{
+    cout << "calling second impl\n";
+    return (first < second) ? second : first;
+}
+
+// template <class T>
+// class
--- COOL STUFF/C++/main.cpp
+++ COOL STUFF/C++/main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
-    cout << biggest(12, 33);
+    cout << biggest<int>(12, 33) << endl;
+    cout << biggest(765, 2) << endl;
+    cout << biggest<int, float>(12, 33.98) << endl;
+    cout << biggest('e', 2) << endl;
 }
