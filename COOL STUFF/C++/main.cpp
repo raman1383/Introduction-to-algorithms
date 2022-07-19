5c7cf288e8fb0c9bf981ebf95f0212da4a2a3fba
raman1383 <raman83shariati@gmail.com>
2022-07-19 13:25:44 +0430

    t

--- COOL STUFF/C++/main.cpp
+++ COOL STUFF/C++/main.cpp
@@ -3,40 +3,6 @@
 #include <memory>
 using std::cout, std::endl, std::vector, std::unique_ptr, std::shared_ptr, std::weak_ptr;
 
-class Rectangle
-{
-    int length;
-    int breadth;
-
-public:
-    Rectangle(int l, int b)
-    {
-        length = l;
-        breadth = b;
-    }
-
-    int area()
-    {
-        return length * breadth;
-    }
-};
-
 int main()
 {
-    shared_ptr<Rectangle> P1(new Rectangle(10, 5));
-    // This'll print 50
-    cout << P1->area() << endl;
-
-    shared_ptr<Rectangle> P2;
-    P2 = P1;
-
-    // This'll print 50
-    cout << P2->area() << endl;
-
-    // This'll now not give an error,
-    cout << P1->area() << endl;
-
-    // This'll also print 50 now
-    // This'll print 2 as Reference Counter is 2
-    cout << P1.use_count() << endl;
 }
