3b948c64502e6d62ec5e3ec6226b533d7f28f180
raman1383 <raman83shariati@gmail.com>
2022-07-22 21:10:01 +0430

    r

--- COOL STUFF/C++/lib.hpp
+++ COOL STUFF/C++/lib.hpp
@@ -1,3 +1,36 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <concepts>
+#include <iterator>
+
+using std::string;
+
+struct Email
+{
+    string subject;
+    string body;
+    string from;
+};
+
+template <typename T>
+concept addable = requires(T obj)
+{
+    obj + obj;
+};
+
+template <typename T>
+concept incrementable = std::regular<T> && std::weakly_incrementable<T> && requires(T t)
+{
+    {
+        t++
+        } -> std::same_as<T>;
+};
+
+template <typename T>
+requires std::integral<T> || std::floating_point<T>
+constexpr double Average(std::vector<T> const &vec)
+{
+    const double sum = std::accumulate(vec.begin(), vec.end(), 0.0);
+    return sum / vec.size();
+}
