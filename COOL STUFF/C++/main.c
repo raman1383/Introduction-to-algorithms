277fe09a443a3ad08b5f345e6d2db9d7beeff748
raman1383 <raman83shariati@gmail.com>
2022-07-23 15:44:07 +0430

    r

--- COOL STUFF/C++/main.c
+++ COOL STUFF/C++/main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-
+void insertionSort(int *arr[]);
 int main()
 {
-    printf("Hello world");
-    for (int i = 0; i < 12; i++)
-        for (int j = 0; j < 12; j++)
-            printf("Hello\n");
+    int arr[] = {1, 432, 43, 52, 532, 6, 2, 72, 742, 22};
+    insertionSort(&arr);
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        printf("%d , ", arr[i]);
+    }
+}
+
+void insertionSort(int **arr)
+{
+    int i = 2;
+    for (; i < sizeof(*arr) / sizeof(int); i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j > 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
