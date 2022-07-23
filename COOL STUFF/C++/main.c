32c11e66f4a4151a64e080fc48be78bbfc0361c7
raman1383 <raman83shariati@gmail.com>
2022-07-23 15:49:37 +0430

    r

--- COOL STUFF/C++/main.c
+++ COOL STUFF/C++/main.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-void insertionSort(int *arr[]);
-int main()
+void printArray(int array[], int size)
 {
-    int arr[] = {1, 432, 43, 52, 532, 6, 2, 72, 742, 22};
-    insertionSort(&arr);
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    for (int i = 0; i < size; i++)
     {
-        printf("%d , ", arr[i]);
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int array[], int size)
+{
+    for (int step = 1; step < size; step++)
+    {
+        int key = array[step];
+        int j = step - 1;
+
+        // Compare key with each element on the left of it until an element smaller than
+        // it is found.
+        // For descending order, change key<array[j] to key>array[j].
+        while (key < array[j] && j >= 0)
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
     }
 }
 
-void insertionSort(int **arr)
+int main()
 {
-    int i = 2;
-    for (; i < sizeof(*arr) / sizeof(int); i++)
-    {
-        int key = arr[i];
-        int j = i - 1;
-        while (j > 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+    int data[] = {9, 5, 1, 4, 3};
+    int size = sizeof(data) / sizeof(data[0]);
+    insertionSort(data, size);
+    printf("Sorted array in ascending order:\n");
+    printArray(data, size);
 }
