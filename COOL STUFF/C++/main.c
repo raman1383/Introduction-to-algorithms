aeae25086576174da22b86153bd110d2de9ad337
raman1383 <raman83shariati@gmail.com>
2022-07-23 14:46:45 +0430

    r

--- COOL STUFF/C++/main.c
+++ COOL STUFF/C++/main.c
@@ -4,10 +4,6 @@ int main()
 {
     printf("Hello world");
     for (int i = 0; i < 12; i++)
-    {
         for (int j = 0; j < 12; j++)
-        {
             printf("Hello\n");
-        }
-    }
 }
