2d07038f080b88278186a0c106326037d1cb3051
raman1383 <raman83shariati@gmail.com>
2022-07-24 14:46:43 +0430

    r

--- leetCode/arrays&hashing/twoSum.py
+++ leetCode/arrays&hashing/twoSum.py
@@ -1,3 +1,15 @@
-dic = {"one": 111, "two": 222}
+def twoSum(nums: list[int], target: int) -> list[int]:
+    prevMap = {}
 
-print(dic.get("one", 0))
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return
+
+
+nums = [2, 11, 15, 7]
+target = 9
+
+print(twoSum(nums, target))
