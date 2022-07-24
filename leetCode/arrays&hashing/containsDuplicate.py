ef83cc48c33923b4dcf1cd1155329f3032dd8a1a
raman1383 <raman83shariati@gmail.com>
2022-07-24 13:30:14 +0430

    r

---
+++ leetCode/arrays&hashing/containsDuplicate.py
@@ -0,0 +1,14 @@
+def containsDuplicate(nums: list[int]) -> bool:
+    hashSet = set()
+
+    for n in nums:
+        if n in hashSet:
+            return True
+        hashSet.add(n)
+    return False
+
+
+nums = [4, 1, 74, 85, 456, 37, 76]
+nums2 = [4, 4, 1, 74, 85, 456, 37, 76]
+print(containsDuplicate(nums))
+print(containsDuplicate(nums2))
